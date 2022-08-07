042a1eba50dc709041aa72375bdfbb554c643f23
MaryAlvarezH <maria.alvarez9409@gmail.com>
2022-08-07 18:35:07 -0500

    replace reference for ItemPreviewType in SearchResults component

--- src/pages/SearchResults/SearchResults.tsx
+++ src/pages/SearchResults/SearchResults.tsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { BASE_URL } from "../../utils/constants";
 import isEmpty from "lodash/isEmpty";
-import { Item } from "../../utils/types";
+import { ItemPreview as ItemPreviewType } from "../../utils/types";
 import { ItemPreview } from "../../components/ItemPreview/ItemPreview";
 import "./styles.scss";
 
 export const SearchResults = () => {
   const [searchParams] = useSearchParams();
-  const [items, setItems] = useState<Item[]>();
+  const [items, setItems] = useState<ItemPreviewType[]>();
 
   useEffect(() => {
     const search: any = searchParams.get("search");
@@ -27,7 +27,7 @@ export const SearchResults = () => {
   return (
     <div className="items-container d-flex flex-column">
       {!isEmpty(items) &&
-        items?.map((i: Item) => {
+        items?.map((i: ItemPreviewType) => {
           return <ItemPreview key={i.id} item={i} />;
         })}
     </div>
