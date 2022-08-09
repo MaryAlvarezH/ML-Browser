97b4cafe456abd50752c6f60f2b837f2487c4e6c
MaryAlvarezH <maria.alvarez9409@gmail.com>
2022-08-08 21:03:44 -0500

    add flag to show CategoriesList component from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { SearchResults } from "./pages/SearchResults/SearchResults";
 import { ItemDetails } from "./pages/ItemDetails/ItemDetails";
-import { Container } from "react-bootstrap";
 import { Navbar } from "./components/Navbar/Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchResults as SearchResultsType } from "./utils/types";
 import { CategoriesList } from "./components/CategoriesList/CategoriesList";
 
 function App() {
+  const [showCategories, setShowCategories] = useState<boolean>(false);
   const [categories, setCategories] = useState<string[]>([]);
+  const location = useLocation();
 
   const onSearchResults = (value: SearchResultsType) => {
     setCategories(value.categories);
   };
 
+  useEffect(() => {
+    if (location.pathname.includes("items")) {
+      setShowCategories(true);
+    } else {
+      setShowCategories(false);
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <Navbar />
 
-      <CategoriesList categories={categories} />
+      {showCategories && <CategoriesList categories={categories} />}
 
-      <Container className="main-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/items"
-            element={<SearchResults onSearchResults={onSearchResults} />}
-          />
-          <Route path="/items/:id" element={<ItemDetails />} />
-        </Routes>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/items"
+          element={<SearchResults onSearchResults={onSearchResults} />}
+        />
+        <Route path="/items/:id" element={<ItemDetails />} />
+      </Routes>
     </>
   );
 }
