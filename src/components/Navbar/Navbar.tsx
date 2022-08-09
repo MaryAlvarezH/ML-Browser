efb4aae9544ab4fab5e61e27085bf157db5a2aa5
MaryAlvarezH <maria.alvarez9409@gmail.com>
2022-08-08 21:01:51 -0500

    add redirection to home page from Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,19 @@
 import { Container } from "react-bootstrap";
 import { SearchBox } from "../SearchBox/SearchBox";
+import { useNavigate } from "react-router-dom";
 import "./styles.scss";
 
 export const Navbar = ({ ...props }) => {
+  const navigate = useNavigate();
+
+  const redirectToHome = () => {
+    navigate("/");
+  };
   return (
     <div className="navbar-container" {...props}>
       <Container>
         <div className="content">
-          <a className="logo" />
+          <a className="logo" onClick={redirectToHome} />
           <SearchBox />
         </div>
       </Container>
