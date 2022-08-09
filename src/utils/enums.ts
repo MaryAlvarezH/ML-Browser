d93ca50198ff01bfcd4cfce2c854221ad968aa77
MaryAlvarezH <maria.alvarez9409@gmail.com>
2022-08-08 21:04:37 -0500

    add enums to handle states and props of components

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,3 +1,20 @@
 export enum ItemConnditions {
   new = "new",
 }
+
+export enum RequestStatus {
+  init,
+  loading,
+  success,
+  error,
+}
+
+export enum SearchTypes {
+  items,
+  itemDetails,
+}
+
+export enum SearchResqueTypes {
+  emptyData,
+  error,
+}
